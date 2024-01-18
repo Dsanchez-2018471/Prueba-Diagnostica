c088278d9f4968f25cd79a962db73d0a8230236a
Dsanchez-2018471 <134979500+Dsanchez-2018461@users.noreply.github.com>
2024-01-18 11:52:06 -0600

    Add files via upload

---
+++ Ejercicio de clase/hombrecito.js
@@ -0,0 +1,57 @@
+const words = [
+    "programming",
+    "javascript",
+    "development",
+    "guatemala",
+    "computer",
+    "technology",
+    "application",
+    "learning",
+    "project",
+    "student",
+    "code",
+    "web",
+    "game",
+    "programmer",
+    "application",
+    "feedback",
+    "career",
+    "challenge",
+    "solution",
+    "algorithm",
+    "function",
+    "bug",
+    "team",
+    "innovation",
+    "framework",
+  ];
+  
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const buttonsContainer = document.getElementById("buttons-container");
+  const usedLettersContainer = document.getElementById("used-letters");
+  const attemptsRemainingContainer =
+    document.getElementById("attempts-remaining");
+  const wordContainer = document.getElementById("word-container");
+  const restartButton = document.getElementById("restart-button");
+  const usedLetters = [];
+  const maxAttempts = 7;
+  const span = document.createElement("span");
+  const canvas = document.getElementById("hangman-canvas");
+  const ctx = canvas.getContext("2d");
+  
+  span.textContent = "RESTART";
+  let attemptsRemaining = maxAttempts;
+  let selectedWord = selectRandomWord(words);
+  let gameInProgress = false;
+  
+  function selectRandomWord(words) {
+    const filteredWords = words.filter((word) => word.length <= 15);
+  
+    if (filteredWords.length === 0) {
+      // No words match the filter criteria
+      return "";
+    }
+  
+    const randomIndex = Math.floor(Math.random() * filteredWords.length);
+    return filteredWords[randomIndex];
+  }
